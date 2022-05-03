8058a4cee7176cdf8655d669f79d6e4e4f15b046
Lester Niel Fong <lester.fong@switchconnect.com.au>
2022-05-03 21:06:25 +0800

    updated

--- Fong_PikalloonGame/Form1.Designer.vb
+++ Fong_PikalloonGame/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,53 +20,34 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.Zapdos = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.Zapdos, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Label1
-        '
-        Me.Label1.AutoEllipsis = True
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(600, 35)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(61, 20)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Score:"
-        '
-        'Label2
-        '
-        Me.Label2.BackColor = System.Drawing.Color.PeachPuff
-        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 17.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(600, 65)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(71, 57)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "0"
-        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
         'Timer1
         '
-        Me.Timer1.Enabled = True
         '
         'Timer2
         '
-        Me.Timer2.Enabled = True
         '
         'Zapdos
         '
@@ -108,18 +89,109 @@ Partial Class Form1
         Me.PictureBox3.TabIndex = 7
         Me.PictureBox3.TabStop = False
         '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.PeachPuff
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 17.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(584, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(192, 57)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "0"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoEllipsis = True
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.PeachPuff
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(585, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Score:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Oswald SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(578, 167)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(160, 32)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Press Enter to Play"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Oswald SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(578, 217)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(179, 32)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Press Space to Pause"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Oswald SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(578, 265)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(159, 32)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Press R to Resume"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Oswald SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(583, 352)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(89, 32)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Difficulty:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Oswald SemiBold", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.MediumPurple
+        Me.Label7.Location = New System.Drawing.Point(583, 384)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 32)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Beginner"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Oswald SemiBold", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(585, 331)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(209, 24)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Press Arrow Up to Adjust Difficulty"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DarkSalmon
         Me.ClientSize = New System.Drawing.Size(800, 585)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Zapdos)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Pikachu Balloon Game"
@@ -133,11 +205,17 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents Timer1 As Timer
     Friend WithEvents Timer2 As Timer
     Friend WithEvents Zapdos As PictureBox
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
 End Class
--- Fong_PikalloonGame/Form1.vb
+++ Fong_PikalloonGame/Form1.vb
@@ -1,52 +1,136 @@
 ﻿Public Class Form1
 
     Dim Score As Integer = 0
+    Dim y As Integer = 1
     Dim Ren As New Random
     Public Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
 
-        PictureBox1.Top -= 15
-        PictureBox2.Top -= 15
-        PictureBox3.Top -= 15
+        PictureBox1.Top -= 8
+        PictureBox2.Top -= 8
+        PictureBox3.Top -= 8
+
+        If (y = 1) Then
+            Label7.Text = "Beginner"
+            Label7.ForeColor = Color.AliceBlue
+            PictureBox1.Top -= 8
+            PictureBox2.Top -= 8
+            PictureBox3.Top -= 8
+        End If
+        If (y = 2) Then
+            Label7.Text = "Normal"
+            Label7.ForeColor = Color.Blue
+            PictureBox1.Top -= 12
+            PictureBox2.Top -= 12
+            PictureBox3.Top -= 12
+        End If
+        If (y = 3) Then
+            Label7.Text = "Insane"
+            Label7.ForeColor = Color.Red
+            PictureBox1.Top -= 20
+            PictureBox2.Top -= 20
+            PictureBox3.Top -= 20
+        End If
+        If (y = 4) Then
+            y = 1
+        End If
 
     End Sub
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
         If (Zapdos.Bounds.IntersectsWith(PictureBox1.Bounds)) Then
             PictureBox1.Top = 560
-            Dim x As Integer = Ren.Next(28, 290)
+            Dim x As Integer = Ren.Next(28, 349)
             PictureBox1.Left = x
             Score += 1
             Label2.Text = Score
         ElseIf (Zapdos.Bounds.IntersectsWith(PictureBox2.Bounds)) Then
             PictureBox2.Top = 560
-            Dim x As Integer = Ren.Next(28, 290)
+            Dim x As Integer = Ren.Next(28, 370)
             PictureBox2.Left = x
             Score += 1
             Label2.Text = Score
         ElseIf (Zapdos.Bounds.IntersectsWith(PictureBox3.Bounds)) Then
             PictureBox3.Top = 560
-            Dim x As Integer = Ren.Next(28, 290)
+            Dim x As Integer = Ren.Next(28, 275)
             PictureBox3.Left = x
             Score += 1
             Label2.Text = Score
         End If
 
         If PictureBox1.Location.Y < -60 Or PictureBox2.Location.Y < -60 Or PictureBox3.Location.Y < -60 Then
-
             Timer1.Enabled = False
             Timer2.Enabled = False
             MessageBox.Show("Game Over, You suck!")
-
         End If
     End Sub
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         If (e.KeyCode = Keys.Right) Then
-            Zapdos.Left = Zapdos.Left + 5
+            Zapdos.Left = Zapdos.Left + 7
         End If
 
         If (e.KeyCode = Keys.Left) Then
-            Zapdos.Left = Zapdos.Left - 5
+            Zapdos.Left = Zapdos.Left - 7
         End If
 
+        If (y = 1) Then
+            If (e.KeyCode = Keys.Left) Then
+                Zapdos.Left = Zapdos.Left - 7
+            End If
+            If (e.KeyCode = Keys.Right) Then
+                Zapdos.Left = Zapdos.Left + 7
+            End If
+        End If
+        If (y = 2) Then
+            If (e.KeyCode = Keys.Left) Then
+                Zapdos.Left = Zapdos.Left - 11
+            End If
+            If (e.KeyCode = Keys.Right) Then
+                Zapdos.Left = Zapdos.Left + 11
+            End If
+        End If
+        If (y = 3) Then
+            If (e.KeyCode = Keys.Left) Then
+                Zapdos.Left = Zapdos.Left - 18
+            End If
+            If (e.KeyCode = Keys.Right) Then
+                Zapdos.Left = Zapdos.Left + 18
+            End If
+        End If
+
+
+        If (e.KeyCode = Keys.Enter) Then
+            Label2.Text = 0
+            Score = 0
+            PictureBox1.Top = 560
+            PictureBox2.Top = 560
+            PictureBox3.Top = 560
+            PictureBox1.Left = 28
+            PictureBox2.Left = 349
+            PictureBox3.Left = 90
+
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+        End If
+
+        If (e.KeyCode = Keys.Space) Then
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+        End If
+        If (e.KeyCode = Keys.R) Then
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+        End If
+
+
+        If (e.KeyCode = Keys.Up) Then
+            y += 1
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("Welcome!")
+        MessageBox.Show("Pop Pikachu's Balloons!")
     End Sub
 
 End Class
